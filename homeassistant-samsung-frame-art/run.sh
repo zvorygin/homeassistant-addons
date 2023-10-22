7a9596acf8f772590a0979c12c5b9caf7cf26a44
Gijs van den Hoven <gijs@vd-hoven.net>
2023-10-22 11:56:56 +0200

    update

--- homeassistant-samsung-frame-art/run.sh
+++ homeassistant-samsung-frame-art/run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Hello world!"
 TVIP=$(bashio::config 'tv')
 
 mkdir -p /media/theframe
-echo "tv-ip ${TVIP}"
+echo "Using ${TVIP} as the IP of the Samsung Frame"
 python3 art.py --tvip ${TVIP}
 echo "done, closing now!"
 kill -s SIGHUP 1
