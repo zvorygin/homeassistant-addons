43a523b1674e2ac0233f9d7593f6a39a047b75c7
Rhys Gray <rhys.gray@itechnologies.com.au>
2024-09-18 08:43:06 +1000

    Refactor, exit early

--- homeassistant-samsung-frametv-artchanger/art.py
+++ homeassistant-samsung-frametv-artchanger/art.py
@@ -56,7 +56,7 @@ if args.debug:
         sys.exit()
     except Exception as e:
         logging.error('Could not reach the TV: ' + str(e))
-        sys.exit()
+        sys.exit(1)
 
 def save_debug_image(image_data, filename):
     if args.debugimage:
@@ -67,57 +67,57 @@ def save_debug_image(image_data, filename):
 # Checks if the TV supports art mode
 art_mode = tv.art().supported()
 
-if art_mode == True:
-    # Retrieve information about the currently selected art
-    current_art = tv.art().get_current()
-
-    sources = []
-    if args.bing_wallpapers:
-        sources.append(BingWallpapers())
-    if args.googleart:
-        sources.append(GoogleArt(args.download_high_res))
-    if args.media_folder:
-        sources.append(MediaFolder(folder_path, uploaded_files, args.upload_all))
-
-    if not sources:
-        logging.error('No image source specified. Please use --googleart, --bing-wallpapers, or --media-folder')
-        sys.exit()
-
-    selected_source = random.choice(sources)
-    logging.info(f'Selected source: {selected_source.__class__.__name__}')
-
-    image_data, file_type, image_info, remote_filename = selected_source.get_image()
-    if image_data is None:
-        sys.exit()
-
-    save_debug_image(image_data, f'debug_{selected_source.__class__.__name__}_original_{image_info}.jpg')
-
-    logging.info('Resizing and cropping the image...')
-    resized_image_data = resize_and_crop_image(image_data)
-
-    save_debug_image(resized_image_data, f'debug_{selected_source.__class__.__name__}_resized_{image_info}.jpg')
-
-    image_data = resized_image_data
-
-    if remote_filename is None:
-        try:
-            logging.info(f'Uploading image')
-            remote_filename = tv.art().upload(image_data.getvalue(), file_type=file_type, matte="none")
-            tv.art().select_image(remote_filename, show=True)
-            logging.info(f'Image uploaded and selected')
-            # Add the filename to the list of uploaded filenames
-            uploaded_files.append({'file': image_info, 'remote_filename': remote_filename})
-            # Save the list of uploaded filenames to the file
-            with open(upload_list_path, 'w') as f:
-                json.dump(uploaded_files, f)
-        except Exception as e:
-            logging.error(f'There was an error uploading the image: ' + str(e))
-            sys.exit()
-    else:
-        if not args.upload_all:
-            # Select the image using the remote file name only if not in 'upload-all' mode
-            logging.info('Setting existing image, skipping upload')
-            tv.art().select_image(remote_filename, show=True)
-
-else:
+if art_mode != True:
     logging.warning('Your TV does not support art mode.')
+    sys.exit(1)
+
+# Retrieve information about the currently selected art
+current_art = tv.art().get_current()
+
+sources = []
+if args.bing_wallpapers:
+    sources.append(BingWallpapers())
+if args.googleart:
+    sources.append(GoogleArt(args.download_high_res))
+if args.media_folder:
+    sources.append(MediaFolder(folder_path, uploaded_files, args.upload_all))
+
+if not sources:
+    logging.error('No image source specified. Please use --googleart, --bing-wallpapers, or --media-folder')
+    sys.exit(1)
+
+selected_source = random.choice(sources)
+logging.info(f'Selected source: {selected_source.__class__.__name__}')
+
+image_data, file_type, image_info, remote_filename = selected_source.get_image()
+if image_data is None:
+    sys.exit(1)
+
+save_debug_image(image_data, f'debug_{selected_source.__class__.__name__}_original_{image_info}.jpg')
+
+logging.info('Resizing and cropping the image...')
+resized_image_data = resize_and_crop_image(image_data)
+
+save_debug_image(resized_image_data, f'debug_{selected_source.__class__.__name__}_resized_{image_info}.jpg')
+
+image_data = resized_image_data
+
+if remote_filename is None:
+    try:
+        logging.info(f'Uploading image')
+        remote_filename = tv.art().upload(image_data.getvalue(), file_type=file_type, matte="none")
+        tv.art().select_image(remote_filename, show=True)
+        logging.info(f'Image uploaded and selected')
+        # Add the filename to the list of uploaded filenames
+        uploaded_files.append({'file': image_info, 'remote_filename': remote_filename})
+        # Save the list of uploaded filenames to the file
+        with open(upload_list_path, 'w') as f:
+            json.dump(uploaded_files, f)
+    except Exception as e:
+        logging.error(f'There was an error uploading the image: ' + str(e))
+        sys.exit()
+else:
+    if not args.upload_all:
+        # Select the image using the remote file name only if not in 'upload-all' mode
+        logging.info('Setting existing image, skipping upload')
+        tv.art().select_image(remote_filename, show=True)
