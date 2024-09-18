ef97f5b8a5d172f646c6013d27512993493dae58
Vivalatech <181739458+vivalatech@users.noreply.github.com>
2024-09-18 20:57:55 +0800

    cleanup config and arguments

--- homeassistant-samsung-frametv-artchanger/run.sh
+++ homeassistant-samsung-frametv-artchanger/run.sh
@@ -7,7 +7,7 @@ echo "Using ${TVIP} as the IP's of the Samsung Frame"
 
 PARAMS=""
 
-if bashio::config.true 'googleart'; then
+if bashio::config.true 'google_art'; then
     PARAMS="${PARAMS} --google-art"
 fi
 if bashio::config.true 'bing_wallpapers'; then
@@ -18,6 +18,9 @@ if bashio::config.true 'media_folder'; then
 fi
 if bashio::config.true 'download_high_res'; then
     PARAMS="${PARAMS} --download-high-res"
+fi
+if bashio::config.true 'same_image'; then
+    PARAMS="${PARAMS} --same-image"
 fi
 
 python3 art.py --tvip ${TVIP} ${PARAMS}
